6f5d63d95c29f48979a20a894494fa6fb1c202f8
liam4 <liamdraws4@gmail.com>
2015-12-04 19:16:06 -0400

    Restore window size on exit

---
+++ clear_prefs.js
@@ -0,0 +1,13 @@
+console.log('Clearing preferences in 10 seconds...');
+console.log('^C to cancel!');
+
+setTimeout(function() {
+  console.log('Clearing preferences in 5 seconds...');
+}, 5000);
+
+setTimeout(function() {
+  console.log('Clearing preferences!');
+
+  var fs = require('fs');
+  fs.writeFileSync(__dirname + '/save/window_size.json', '{}');
+}, 10000);
--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 var app = require('app');
+var fs = require('fs');
 var BrowserWindow = require('browser-window');
 var mainWindow = null;
 
 app.on('window-all-closed', function() {
+  var size = mainWindow.getContentSize();
+  var width = size[0];
+  var height = size[1];
+  var obj = {width: width, height: height};
+  var objStr = JSON.stringify(obj);
+  fs.writeFileSync(__dirname + '/save/window_size.json', objStr);
   app.quit();
 });
 
 app.on('ready', function() {
-  mainWindow = new BrowserWindow({width: 1024, height: 768 });
-  mainWindow.loadUrl('file://' + __dirname + '/browser/browser.html');
+  fs.readFile(__dirname + '/save/window_size.json', function(err, size) {
+    size = JSON.parse(size);
+    mainWindow = new BrowserWindow({width: size.width, height: size.height});
+    mainWindow.loadUrl('file://' + __dirname + '/browser/browser.html');
+  });
   //mainWindow.openDevTools();
 });
